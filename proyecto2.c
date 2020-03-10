bab90961a5a09692431ae08dfdce7900d676f4b4
lucasyabby <61995785+lucasyabby@users.noreply.github.com>
2020-03-09 21:02:32 -0300

    proyecto2

---
+++ proyecto2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor hora=0;
+    int cantidad de horas=0;
+    int impuestos=0;
+
+    printf("ingrese su nombre");
+    printf("ingrese valor de su hora");
+    scanf("%d",&valor hora);
+    printf("ingrese cantidad de horas");
+    scanf("%d",&cantidad de horas);
+    return 0;
+}
